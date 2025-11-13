17a34495fe2e999a79e3a6ce486506ab5686f1af
voicesp <admystro@gmail.com>
2025-11-13 12:47:06 +0200

    convert video

--- gulpfile.js
+++ gulpfile.js
@@ -15,25 +15,25 @@ gulp.task("server", function () {
 
 gulp.task("styles", function () {
   return gulp.src("src/sass/*.+(scss|sass)")
-    .pipe(sass({style: 'compressed'}).on('error', sass.logError))
+    .pipe(sass({ style: 'compressed' }).on('error', sass.logError))
     .pipe(rename({
-      prefix:'',
-      suffix:'.min'
+      prefix: '',
+      suffix: '.min'
     }))
     .pipe(autoprefixer({
-      browsers:['last 2 versions'],
-      cascade:true
+      overrideBrowserslist: ['last 2 versions'],
+      cascade: true
     }))
     .pipe(cleanCss({
-      compatibility:'ie8'
+      compatibility: 'ie8'
     }))
     .pipe(gulp.dest("src/css"))
     .pipe(browserSync.stream())
 })
 
 gulp.task('watch', function () {
-  gulp.watch('src/sass/*.+(scss|sass)',gulp.parallel('styles'))
-  gulp.watch('src/*.html').on('change',browserSync.reload)
+  gulp.watch('src/sass/*.+(scss|sass)', gulp.parallel('styles'))
+  gulp.watch('src/*.html').on('change', browserSync.reload)
 })
 
-gulp.task('default',gulp.parallel('watch','server','styles'))
+gulp.task('default', gulp.parallel('watch', 'server', 'styles'))
